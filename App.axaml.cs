38deee13f0d178a1020f591a31bdfa4072286607
lemon <dkhalid.bese24seecs@seecs.edu.pk>
2025-05-16 02:43:55 +0500

    Initial Commit.

---
+++ App.axaml.cs
@@ -0,0 +1,55 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using System.Linq;
+using Avalonia.Markup.Xaml;
+using IRis.Services;
+using IRis.ViewModels;
+using IRis.Views;
+
+namespace IRis;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        // NOTE: IMPLEMENT A THEME SYSTEM
+        // Set the default theme as 'Dark'
+        // ThemeManager.ChangeTheme(Application.Current, "Dark");
+        
+       // Create a CanvasService object and pass its reference to both MainWindow and MainWindowViewModel
+       CanvasService canvasService = new CanvasService();
+        
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+            DisableAvaloniaDataAnnotationValidation();
+            desktop.MainWindow = new MainWindow(canvasService)
+            {
+                DataContext = new MainWindowViewModel(canvasService),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        var dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+        // remove each entry found
+        foreach (var plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
+}
