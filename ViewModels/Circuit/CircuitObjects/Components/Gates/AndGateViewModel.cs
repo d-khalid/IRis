8fcca57af8237ebe804a17348717397474b0d68b
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-05-17 12:32:52 +0500

    add input and output terminals to AND gate

--- ViewModels/Circuit/CircuitObjects/Components/Gates/AndGateViewModel.cs
+++ ViewModels/Circuit/CircuitObjects/Components/Gates/AndGateViewModel.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.Generic;
 using IRis.ViewModels.Circuit.CircuitObjects.Core;
+using System.Collections.ObjectModel;
 
 
 namespace IRis.ViewModels.Circuit.CircuitObjects.Components.Gates;
@@ -8,6 +9,12 @@ namespace IRis.ViewModels.Circuit.CircuitObjects.Components.Gates;
 
 public partial class AndGateViewModel : GateViewModel
 {
-    // public readonly List<TerminalViewModel> Inputs = inputs;
-}
+    public ObservableCollection<TerminalViewModel> Inputs { get; } = [];
 
+    
+    public void AddInput(TerminalViewModel input)
+    {
+        Inputs.Add(input);
+        Width = Height = Inputs.Count * 20;
+    }
+}
--- ViewModels/Main/LeftSidebarViewModel.cs
+++ ViewModels/Main/LeftSidebarViewModel.cs
@@ -4,6 +4,8 @@ using IRis.ViewModels;
 using IRis.ViewModels.Circuit.CircuitObjects.Components.Gates;
 using Tmds.DBus.Protocol;
 using Avalonia;
+using IRis.ViewModels.Circuit.CircuitObjects.Core;
+using IRis.Models.Core;
 
 
 namespace IRis.ViewModels.Main;
@@ -18,12 +20,14 @@ public partial class LeftSidebarViewModel : ViewModelBase
     [RelayCommand]
     private void AddAnd()
     {
-        Simulation.PreviewObjects.Add(new AndGateViewModel() { 
-            Width = 50,
-            Height = 50,
-            Opacity = 0.5
-        });
+        TerminalViewModel i1 = new(new Terminal(TerminalType.Input));
+        TerminalViewModel i2 = new(new Terminal(TerminalType.Input));
 
+        AndGateViewModel gate = new() { Opacity = 0.5 };
+
+        gate.AddInput(i1);
+        gate.AddInput(i2);
+        Simulation.PreviewObjects.Add(gate);
         Simulation.PreviewMouseOffset = new Point(25, 25);
     }
 }
