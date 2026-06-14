e7e657861cff97c90a673517b7734e640dc4cb7b
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-06-14 15:36:53 +0500

    add logic for nor,nand,xnor gates

---
+++ Models/CircuitObjects/Components/Gates/NorGate.cs
@@ -0,0 +1,37 @@
+using IRis.Models.Core;
+
+
+namespace IRis.Models.CircuitObjects.Components.Gates;
+
+
+public class NorGate : MultiInputGate
+{
+    public override void Simulate()
+    {
+        foreach (var i in Inputs)
+        {
+            if (i.State == LogicState.Unknown)
+            {
+                Output.State = LogicState.Unknown;
+                return;
+            }
+        }
+
+        foreach (var i in Inputs)
+        {
+            if (i.State == LogicState.High)
+            {
+                Output.State = LogicState.Low;
+                return;
+            }
+        }
+
+        Output.State = LogicState.High;
+    }
+
+
+    public override void Reset()
+    {
+        Output.State = LogicState.Unknown;
+    }
+}
