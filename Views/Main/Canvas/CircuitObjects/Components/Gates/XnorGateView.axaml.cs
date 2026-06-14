fe746acf9ac5ada0e7d8d63599862c352d04dea3
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-06-14 15:38:33 +0500

    wire up remaining gates

---
+++ Views/Main/Canvas/CircuitObjects/Components/Gates/XnorGateView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+
+namespace IRis.Views.Main.Canvas.CircuitObjects.Components.Gates;
+
+
+public partial class XnorGateView : UserControl
+{
+    public XnorGateView()
+    {
+        InitializeComponent();
+    }
+}
