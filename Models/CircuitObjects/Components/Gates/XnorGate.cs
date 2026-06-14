b713d265d75253af2ff59a11e9e5242374e44c73
Shahzaib Ahmad <shahzaibahmad6789@gmail.com>
2026-06-14 15:55:05 +0500

    add NOR, NAND, XNOR gates (#53)
    * add logic for nor,nand,xnor gates
    
    * create stubs for adding nor,nand,xnor
    
    * wire up remaining gates
    
    * add more width and move not bubble forward

---
+++ Models/CircuitObjects/Components/Gates/XnorGate.cs
@@ -0,0 +1,36 @@
+using IRis.Models.Core;
+
+
+namespace IRis.Models.CircuitObjects.Components.Gates;
+
+
+public class XnorGate : MultiInputGate
+{
+    public override void Simulate()
+    {
+        foreach (var i in Inputs)
+        {
+            if (i.State == LogicState.Unknown)
+            {
+                Output.State = LogicState.Unknown;
+                return;
+            }
+        }
+
+        int highs = 0;
+
+        foreach (var i in Inputs)
+        {
+            if (i.State == LogicState.High)
+                highs++;
+        }
+
+        Output.State = highs % 2 == 0 ? LogicState.High : LogicState.Low;
+    }
+
+
+    public override void Reset()
+    {
+        Output.State = LogicState.Unknown;
+    }
+}
---
+++ Views/Main/Canvas/CircuitObjects/Components/Gates/NandGateView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+
+namespace IRis.Views.Main.Canvas.CircuitObjects.Components.Gates;
+
+
+public partial class NandGateView : UserControl
+{
+    public NandGateView()
+    {
+        InitializeComponent();
+    }
+}
