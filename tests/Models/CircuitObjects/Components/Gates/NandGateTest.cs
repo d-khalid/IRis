de39b6abcf5b4cd628fcf6b254d33af485f5d2cd
Shahzaib Ahmad Shahid <shahzaibahmad6789@gmail.com>
2026-07-04 12:42:59 +0500

    refac: fix unnecessarily nested tests (#77)
    * document testing process
    
    * add tests for all current components
    
    * wire up testing in ci
    
    * refac: un-nest structure for tests
    
    * correct commands for testing

---
+++ tests/Models/CircuitObjects/Components/Gates/NandGateTest.cs
@@ -0,0 +1,25 @@
+using IRis.Models.CircuitObjects.Components.Gates;
+using IRis.Models.Core;
+using Xunit;
+
+namespace IRis.Tests.Models.CircuitObjects.Components.Gates;
+
+public class NandGateTest
+{
+    [Theory]
+    [InlineData(LogicState.High, LogicState.High, LogicState.Low)]
+    [InlineData(LogicState.High, LogicState.Low, LogicState.High)]
+    [InlineData(LogicState.Low, LogicState.High, LogicState.High)]
+    [InlineData(LogicState.Low, LogicState.Low, LogicState.High)]
+    [InlineData(LogicState.Unknown, LogicState.High, LogicState.Unknown)]
+    public void SimulateExampleScenarios(LogicState a, LogicState b, LogicState expected)
+    {
+        var gate = new NandGate { Output = new Terminal() };
+        gate.Inputs.Add(new Terminal { State = a });
+        gate.Inputs.Add(new Terminal { State = b });
+
+        gate.Simulate();
+
+        Assert.Equal(expected, gate.Output.State);
+    }
+}
---
+++ tests/Models/CircuitObjects/Components/Gates/XnorGateTest.cs
@@ -0,0 +1,24 @@
+using IRis.Models.CircuitObjects.Components.Gates;
+using IRis.Models.Core;
+using Xunit;
+
+namespace IRis.Tests.Models.CircuitObjects.Components.Gates;
+
+public class XnorGateTest
+{
+    [Theory]
+    [InlineData(LogicState.High, LogicState.High, LogicState.High)]
+    [InlineData(LogicState.High, LogicState.Low, LogicState.Low)]
+    [InlineData(LogicState.Low, LogicState.Low, LogicState.High)]
+    [InlineData(LogicState.Unknown, LogicState.High, LogicState.Unknown)]
+    public void SimulateExampleScenarios(LogicState a, LogicState b, LogicState expected)
+    {
+        var gate = new XnorGate { Output = new Terminal() };
+        gate.Inputs.Add(new Terminal { State = a });
+        gate.Inputs.Add(new Terminal { State = b });
+
+        gate.Simulate();
+
+        Assert.Equal(expected, gate.Output.State);
+    }
+}
