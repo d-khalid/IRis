aae926538fc860fc23a7c62b3a4a57af8c0038cd
Shahzaib Ahmad Shahid <shahzaibahmad6789@gmail.com>
2026-07-13 18:01:36 +0500

    feat: state, output for mux, gates, io (#119)

--- src/ViewModels/Main/RightSidebarViewModel.cs
+++ src/ViewModels/Main/RightSidebarViewModel.cs
@@ -1,4 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using IRis.Models.CircuitObjects.Components;
+using IRis.Models.Core;
 using IRis.Services.Singleton;
 using IRis.ViewModels.Main.Canvas.CircuitObjects;
 using IRis.ViewModels.Main.Canvas.CircuitObjects.Components;
@@ -11,16 +13,16 @@ public partial class RightSidebarViewModel : ViewModelBase
     private readonly Selection _selection;
 
     [ObservableProperty]
-    private bool _isVisible;
+    private bool _isVisible = false;
 
     [ObservableProperty]
-    private ComponentViewModel? _selectedComponent;
+    private ComponentViewModel? _selectedComponent = null;
 
     [ObservableProperty]
     private string _componentType = "";
 
     [ObservableProperty]
-    private bool _isMultiInputVisible;
+    private bool _isMultiInputVisible = false;
 
     [ObservableProperty]
     private string _dynamicPinsCount = "";
@@ -29,10 +31,28 @@ public partial class RightSidebarViewModel : ViewModelBase
     private string _rotation = "";
 
     [ObservableProperty]
-    private bool _isClockVisible;
+    private bool _isClockVisible = false;
 
     [ObservableProperty]
     private string _frequencyText = "";
+
+    [ObservableProperty]
+    private bool _isToggleVisible = false;
+
+    [ObservableProperty]
+    private string _toggleState = "";
+
+    [ObservableProperty]
+    private bool _isOutputVisible = false;
+
+    [ObservableProperty]
+    private string _outputState = "";
+
+    [ObservableProperty]
+    private bool _isProbeVisible = false;
+
+    [ObservableProperty]
+    private string _probeState = "";
 
     public RightSidebarViewModel(Selection selection)
     {
@@ -43,6 +63,9 @@ public partial class RightSidebarViewModel : ViewModelBase
             IsVisible = false;
             IsMultiInputVisible = false;
             IsClockVisible = false;
+            IsToggleVisible = false;
+            IsOutputVisible = false;
+            IsProbeVisible = false;
 
             if (_selection.Objects.Count == 1 && _selection.Objects[0] is ComponentViewModel co)
             {
@@ -74,6 +97,47 @@ public partial class RightSidebarViewModel : ViewModelBase
                 {
                     IsClockVisible = true;
                     FrequencyText = clock.FrequencyHz.ToString();
+                }
+                else if (co is ToggleViewModel toggle)
+                {
+                    IsToggleVisible = true;
+                    ToggleState = toggle.State.ToString();
+
+                    toggle.PropertyChanged += (_, e) =>
+                    {
+                        if (e.PropertyName == nameof(ToggleViewModel.State))
+                        {
+                            ToggleState = toggle.State.ToString();
+                        }
+                    };
+                }
+                else if (co is ProbeViewModel probe)
+                {
+                    IsProbeVisible = true;
+                    ProbeState = probe.State.ToString();
+
+                    probe.PropertyChanged += (_, e) =>
+                    {
+                        if (e.PropertyName == nameof(ProbeViewModel.State))
+                        {
+                            ProbeState = probe.State.ToString();
+                        }
+                    };
+                }
+
+                var output = (co as GateViewModel)?.Output ?? (co as MultiplexerViewModel)?.Output;
+                if (output is not null)
+                {
+                    IsOutputVisible = true;
+                    OutputState = output.GetModel().State.ToString();
+
+                    output.PropertyChanged += (_, e) =>
+                    {
+                        if (e.PropertyName == nameof(Terminal.State))
+                        {
+                            OutputState = output.GetModel().State.ToString();
+                        }
+                    };
                 }
             }
         };
