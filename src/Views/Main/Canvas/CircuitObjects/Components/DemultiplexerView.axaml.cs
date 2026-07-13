334cfffda874920465e59f982a7350fb8732109a
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-07-13 16:37:09 +0500

    ui: remake demux component as a trapezium

--- src/Views/Main/Canvas/CircuitObjects/Components/DemultiplexerView.axaml.cs
+++ src/Views/Main/Canvas/CircuitObjects/Components/DemultiplexerView.axaml.cs
@@ -1,11 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
 
 namespace IRis.Views.Main.Canvas.CircuitObjects.Components;
 
 public partial class DemultiplexerView : UserControl
 {
+    public static readonly IMultiValueConverter CanvasOffset = new SelectCanvasOffsetConverter();
+
     public DemultiplexerView()
     {
         InitializeComponent();
+    }
+
+    // DISCLAIMER: this is AI-generated, binds in the axaml
+    private sealed class SelectCanvasOffsetConverter : IMultiValueConverter
+    {
+        public object? Convert(
+            IList<object?> values,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            if (values.Count < 2 || values.Any(v => v is not double))
+                return 0.0;
+
+            return (double)values[0]! - (double)values[1]! - 5;
+        }
     }
 }
